05d87b2591ff314a3d7b295fbed2bdfc7cb987b6
IlyaLaska <IlyaLaska@users.noreply.github.com>
2021-04-30 00:37:45 +0300

    feat: Complete initial setup

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import * as appRoot from 'app-root-path';
+import * as fs from 'fs';
+const packageJson = JSON.parse(fs.readFileSync(`${appRoot.toString()}/package.json`, 'utf8'));
+const healthResponse = JSON.stringify(
+  {
+    version: packageJson.version,
+    description: packageJson.description,
+    author: packageJson.author,
+  },
+  null,
+  2,
+);
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  health(): string {
+    return healthResponse;
   }
 }
